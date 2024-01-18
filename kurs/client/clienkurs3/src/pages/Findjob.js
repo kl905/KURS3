68b4e7571ba20f86c3f8eba8c7d99822e7192be1
kl905 <95036820+kl905@users.noreply.github.com>
2024-01-18 22:56:23 +0500

    18.01.2024 3

--- kurs/client/clienkurs3/src/pages/Findjob.js
+++ kurs/client/clienkurs3/src/pages/Findjob.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {Form, Row} from "react-bootstrap";
+import {Button, Card, Container, Form, Image, Row} from "react-bootstrap";
 import {useState} from "react";
 import '../compstyles/FindJobStyles.css'
 import CfpItem from "../components/CfpItem";
 import CfpList from "../components/CfpList";
+import {NavLink, Outlet} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
+import A1 from "../assets/A1.jpg";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import {Slider,RangeSlider} from "rsuite";
+
+
 
 
 
@@ -21,6 +28,7 @@ const Findjob = () => {
     }
 
     return (
+
         <Form>
             <div className='p-2'>
                 <select value={Cl} onChange={e=>setCl(e.target.value)}>
@@ -34,8 +42,79 @@ const Findjob = () => {
             </div>
         </Form>
 
+        /*
+        <Container className="d-flex justify-content-center align-items-center"
+                   style = {{height:window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">Регистрация</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите ваш email..."
 
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите ваш пароль..."
+                        type="password"
+                    />
+                    <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                        <Button variant="outline-dark" className="mb-2 ml-2">
+                            Регистрация
+                        </Button>
+                    </Row>
+                </Form>
+            </Card>
+        </Container>
+         */
+        /*
+        <Container className="d-flex justify-content-center align-items-center"
+                   style = {{height:window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">Регистрация</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите Ф.И.О. родителя..."
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите login родителя..."
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите Ф.И.О. ученика..."
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите login ученика..."
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите пароль ..."
+                        type="password"
+                    />
+                    <div className='mt-3'>
+                        <select>
+                            <option value="A-1">A-1</option>
+                            <option value="A-2">A-2</option>
+                            <option value="A-3">A-3</option>
+                        </select>
+                    </div>
+                    <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                        <Button variant="outline-dark" className="mb-2 ml-2">
+                            Ещё один выживший из ума
+                        </Button>
+                    </Row>
+                </Form>
+            </Card>
+        </Container>
+         */
     );
+
+
 };
 
 export default Findjob;
